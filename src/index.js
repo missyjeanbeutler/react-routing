36e0b63e61e7a60b57cfeec1c374f791a9ad61ce
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-08-21 20:40:53 -0600

    inital prep

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>, 
+  </HashRouter>, 
   document.getElementById('root')
 );
 registerServiceWorker();
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Store from './components/Store/Store.js';
+import Details from '.;./components/Details/Details.js';
+import Checkout from './components/Checkout/Checkout.js';
+
+
+export default (
+  <Switch>
+    <Route exact path='/' component={Store} />
+    <Route path='/details/:id' component={Details} />
+    <Route path='/checkout' component={Checkout} />
+  </Switch>
+)

c6121a64a85f281083266f462e2403654b4b634c
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-08-21 21:17:17 -0600

    prepping adjustments

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Store from './components/Store/Store.js';
-import Details from '.;./components/Details/Details.js';
+import Details from './components/Details/Details.js';
 import Checkout from './components/Checkout/Checkout.js';
 
 

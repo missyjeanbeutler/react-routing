f259d5c11813b177e299c6ab925f599dce56aabf
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-08-21 21:47:20 -0600

    finished prep

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { addToCart } from "../../ducks/reducer";
 import { connect } from "react-redux";
-
+import { Link } from 'react-router-dom';
 import './Details.css';
 
 function Details( { swag, history, addToCart } ) {
@@ -13,7 +13,9 @@ function Details( { swag, history, addToCart } ) {
 
   return (
     <div id="Details__container">
-      <p> { '<' } Back to store </p>
+      <Link to='/' id='Details__storeLink'>
+        <p> { '<' } Back to store </p>
+      </Link>
       <img id="Details__img" src={ swag.img } alt="Product" width="400px" />
       <p id="Details__title"> { swag.title } </p>
       <p id="Details__price"> ${ swag.price } </p>
